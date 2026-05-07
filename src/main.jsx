550b6b35601e08dafcdeeec40259f77d2a9d94c2
sigurdjosok <sigurd.jos@gmail.com>
2026-05-07 15:49:57 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import NorgeKart from "./NorgeKart.jsx";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <NorgeKart />
+    <App />
   </React.StrictMode>
 );
