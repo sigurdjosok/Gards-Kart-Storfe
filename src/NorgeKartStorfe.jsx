277afdeb42fc3b09d230c7832d9af6efb7480435
sigurdjosok <sigurd.jos@gmail.com>
2026-05-08 13:49:24 +0200

    Update NorgeKartStorfe.jsx

--- src/NorgeKartStorfe.jsx
+++ src/NorgeKartStorfe.jsx
@@ -182,7 +182,7 @@ export default function NorgeKartStorfe() {
   return (
     <div className="w-full h-[750px] grid grid-cols-1 lg:grid-cols-3 gap-3">
       <div className="lg:col-span-1 p-3">
-        <div className="text-xl font-semibold">🐖 Storfebønder</div>
+        <div className="text-xl font-semibold">🐂 Storfebønder</div>
         <div className="text-sm mt-1">{loadInfo}</div>
 
         <input
